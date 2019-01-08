f1804298b21fbbf0a7bb34046b9b8f05cd82dd8a
da Vinci <brucewilson1103@gmail.com>
2019-01-08 09:41:08 -0500

    retry

--- assets/rps.js
+++ assets/rps.js
@@ -1,6 +1,6 @@
  // Initialize Firebase
  var config = {
-   apiKey: "",
+   apiKey: "AIzaSyCR3fGoUlWoWUokNdozkhrKvlrBfOCNBF8",
    authDomain: "rock-paper-scissors-a9000.firebaseapp.com",
    databaseURL: "https://rock-paper-scissors-a9000.firebaseio.com",
    projectId: "rock-paper-scissors-a9000",
